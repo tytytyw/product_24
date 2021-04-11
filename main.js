4a6bb7bda750ba6e248c7f1fa3f606976c78c88d
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2021-04-11 19:37:08 +0300

    feat: mobile-320px

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
+var dropDownElements = document.querySelectorAll('.drop-down__item');
+var feedBackCardElements = document.querySelectorAll('.feedback__card');
+
 $('.slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    mobileFirst: true,
-     responsive: [
-        {
-           breakpoint: 1023,
-           settings: "unslick"
-        }
-     ]
-  });
+   dots: true,
+   infinite: true,
+
+   speed: 300,
+   slidesToShow: 1,
+
+   adaptiveHeight: true,
+   mobileFirst: true,
+   responsive: [
+      {
+         breakpoint: 1023,
+         settings: "unslick"
+      }
+   ]
+});
+
+function dropDownClick () {
+   function classModify (e) {
+      var activeEl = document.querySelector('.drop-down__item.active');
+      var currentEl = e.currentTarget;
+      if (activeEl) {
+         activeEl.classList.remove('active');
+      }
+
+      if (activeEl !== currentEl) {
+         currentEl.classList.add("active")
+      }
+      return false
+   }
+
+   for (var i = 0; i < dropDownElements.length; i++) {
+      dropDownElements[i].addEventListener('click', classModify)
+   }
+}
+dropDownClick()
+
+function feedbackItemClick () {
+   function classModify (e) {
+      var activeEl = document.querySelector('.feedback__card.active');
+      var currentEl = e.currentTarget
+      
+      if (currentEl.className.split(" ")[1] === 'reverse') {
+         activeEl.classList.add('reverse');
+         currentEl.classList.remove('reverse')
+      }
+      activeEl.classList.remove('active');
+      currentEl.classList.add("active")
+      return false
+   }
+   for (var i = 0; i < feedBackCardElements.length; i++) {
+      feedBackCardElements[i].addEventListener('click', classModify)
+   }
+}
+feedbackItemClick()
